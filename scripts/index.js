3658ec12457839e4229d97d0aef2e1ef697aa2bf
valva-ro <vavarela.rodriguez@gmail.com>
2021-06-21 12:16:00 -0300

    [ADD] possible resolution

--- scripts/index.js
+++ scripts/index.js
@@ -2,14 +2,49 @@ const registerBtn = document.querySelector(".register-btn");
 
 registerBtn.addEventListener("click", () => {
   // Escribe tu código aquí, siguiendo los siguientes lineamientos paso a paso:
+ 
   // 1. Obtenemos el valor ingresado en el input de email
+  const mail = document.querySelector("#email-input").value;
+
   // 2. Obtenemos los datos ingresados en el input de password
+  const pswd = document.querySelector("#password-input").value;
+
   // 3. Obtenemos el valor del input radio
+  const esMayorDeEdad = document.querySelector("#age_yes").checked;
+
   // 4. Obtenemos el valor del input checkbox
-  // 5 Validamos si el usuario es mayor de edad. Si no, mostramos
+  const aceptaTyC = document.querySelector("#tyc-input").checked;
+
+  // 5. Validamos si el usuario es mayor de edad. Si no, mostramos
   // un mensaje de error: "Debes ser mayor de edad para registrarte en el sitio"
-  // 6 Vaidamos si el usuario aceptó los términos y condiciones. Si no, mostramos
+  validarEdad(esMayorDeEdad);
+
+  // 6. Validamos si el usuario aceptó los términos y condiciones. Si no, mostramos
   // un mensaje de error: "Debes aceptar los TyCs para registrarte en el sitio"
-  // 7 Si todo esta correcto, mostramos por consola un objeto con la información
+  validarTyC(aceptaTyC);
+
+  // 7. Si todo esta correcto, mostramos por consola un objeto con la información
   // ingresada por el usuario.
+  const usuario = {
+    email: mail,
+    password: pswd,
+    esMayorDeEdad: esMayorDeEdad,
+    aceptaTyC: aceptaTyC,
+  }
+  validarInformacion(usuario);
+  
 });
+
+function validarEdad(esMayorDeEdad) {
+  return esMayorDeEdad ? true : alert("Debes ser mayor de edad para registrarte en el sitio");
+}
+
+function validarTyC(aceptaTyC) {
+  return aceptaTyC ? true : alert("Debes aceptar los TyCs para registrarte en el sitio");
+}
+
+function validarInformacion(usuario) {
+  if (usuario.email && usuario.password && usuario.esMayorDeEdad && usuario.aceptaTyC) {
+    console.log(usuario);
+  }
+}
