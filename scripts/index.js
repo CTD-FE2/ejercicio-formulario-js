f593ee3a29252d6a699f32a3d229b3a9a6f3451c
valva-ro <vavarela.rodriguez@gmail.com>
2021-06-23 16:13:34 -0300

    [REFACTOR] rename function and reorder comments

--- scripts/index.js
+++ scripts/index.js
@@ -4,23 +4,19 @@ registerBtn.addEventListener("click", () => {
   // Escribe tu código aquí, siguiendo los siguientes lineamientos paso a paso:
  
   // 1. Obtenemos el valor ingresado en el input de email
-  const mail = document.querySelector("#email-input").value;
-
   // 2. Obtenemos los datos ingresados en el input de password
-  const pswd = document.querySelector("#password-input").value;
-
   // 3. Obtenemos el valor del input radio
-  const esMayorDeEdad = document.querySelector("#age_yes").checked;
-
   // 4. Obtenemos el valor del input checkbox
+  const mail = document.querySelector("#email-input").value;
+  const pswd = document.querySelector("#password-input").value;
+  const esMayorDeEdad = document.querySelector("#age_yes").checked;
   const aceptaTyC = document.querySelector("#tyc-input").checked;
 
   // 5. Validamos si el usuario es mayor de edad. Si no, mostramos
   // un mensaje de error: "Debes ser mayor de edad para registrarte en el sitio"
   validarEdad(esMayorDeEdad);
-
   // 6. Validamos si el usuario aceptó los términos y condiciones. Si no, mostramos
-  // un mensaje de error: "Debes aceptar los TyCs para registrarte en el sitio"
+  // un mensaje de error: "Debes aceptar los TyCs para registrarte en el sitio"  
   validarTyC(aceptaTyC);
 
   // 7. Si todo esta correcto, mostramos por consola un objeto con la información
@@ -31,8 +27,7 @@ registerBtn.addEventListener("click", () => {
     esMayorDeEdad: esMayorDeEdad,
     aceptaTyC: aceptaTyC,
   }
-  validarInformacion(usuario);
-  
+  mostrarInformacion(usuario);
 });
 
 function validarEdad(esMayorDeEdad) {
@@ -43,7 +38,7 @@ function validarTyC(aceptaTyC) {
   return aceptaTyC ? true : alert("Debes aceptar los TyCs para registrarte en el sitio");
 }
 
-function validarInformacion(usuario) {
+function mostrarInformacion(usuario) {
   if (usuario.email && usuario.password && usuario.esMayorDeEdad && usuario.aceptaTyC) {
     console.log(usuario);
   }
